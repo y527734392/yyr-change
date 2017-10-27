26907320d2e8f130fd0bc74537deb4a2cd9ac839
power-lgl <871228656@qq.com>
2017-10-27 16:20:40 +0800

    新建目录

---
+++ app/containers/show/Criteria/index.jsx
@@ -0,0 +1,86 @@
+/**
+ * Created by lgl on 17/10/13.
+ */
+
+/**
+ * react component
+ */
+import React from 'react'
+import { Link } from 'react-router'
+
+/**
+ * css
+ */
+import 'less/containers/Show';
+
+//content
+class Criteria extends React.Component {
+	constructor(props,context){
+		super(props,context);
+		this.state = {
+
+
+		}
+	}
+	render() {
+		var city_items = [],
+			year_items = [],
+			month_items = [],
+			min_year = '0',
+			month_list = ['01','02','03','04','05','06','07','08','09','10','11','12',],
+			cur_city_code = this.props['yt-data-criteria']['city_info']['cur_city'].code;
+		this.props['yt-data-criteria']['city_info'].list.map((list,index)=>{
+			if(cur_city_code !== list.cityentry[0].code){
+				city_items.push(<li key={index}>{list.cityentry[0].name}</li>)
+			}else{
+				city_items.push(<li key={index} className="on">{list.cityentry[0].name}</li>)
+			}
+
+		});
+		for(var year in this.props['yt-data-criteria']['year_month']){
+			console.log(min_year>=year);
+			if(min_year>=year){
+				min_year = year;
+			}
+			year_items.push(<li key={year}>{year}</li>)
+		}
+		console.log(min_year);
+		for(var month in month_list){
+			console.log(month);
+			if(this.props['yt-data-criteria']['year_month']['2017'][month] === 1){
+				month_items.push(<li className="on" key={month}>{month_list[month]}月</li>)
+			}else{
+				month_items.push(<li key={month}>{month_list[month]}月</li>)
+			}
+		}
+
+		return (
+			<div className="Criteria_info">
+				<div className="city_info clearfix">
+					<p>演出城市</p>
+					<ul className="city_list">
+						{city_items}
+					</ul>
+
+				</div>
+				<div className="date_info clearfix">
+					<p>演出时间</p>
+					<div className="date_con">
+						<ul className="year_list">
+							{year_items}
+						</ul>
+						<ul className="month_list">
+							{month_items}
+						</ul>
+					</div>
+
+
+				</div>
+
+			</div>
+		)
+	}
+
+}
+export default Criteria
+
